c7b41481e20be47e362319399e09a0c3727480d3
Zokof <vip.salm2@gmail.com>
2021-09-27 13:29:04 +0300

    Card component with state was added// search function was done

--- src/App.js
+++ src/App.js
@@ -1,52 +1,65 @@
 import React from "react";
-import { render } from "react-dom";
 import "./App.css";
 
-function ShowListProd(props) {
+function Cards(props) {
   console.log("props.props", props.data); // list with arrays
-  return props.data.map((item) => {
-    if (props.arrIds.includes(item.id)) {
-      return (
-        <CardFullInfo
-          name={item.name}
-          description={item.description}
-          abv={item.abv}
-          contributed_by={item.contributed_by}
-          brewers_tips={item.brewers_tips}
-          first_brewed={item.first_brewed}
-          food_pairing={item.food_pairing}
-          ibu={item.ibu}
-          ph={item.ph}
-          srm={item.srm}
-          tagline={item.tagline}
-          onClick={() => props.onClickBack(item.id)}
-        />
-      );
-    } else {
-      return (
-        <Card
-          name={item.name}
-          image_url={item.image_url}
-          description={item.description}
-          onClick={() => props.onClickShowDetails(item.id)}
-        />
-      );
-    }
-  });
+  return props.data.map((item) => <Card key={item.id} item={item}/>);
 }
 
-function Card(props) {
+class Card extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      isDefault: true,
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState((state) => ({isDefault: !state.isDefault}))
+  }
+
+  render(){
+    const {item} = this.props;
+
+    return (
+      <div className="card">
+        <button className="showButton" onClick={this.toggle}>
+          {this.state.isDefault ? "Show Full Info" : "Back"} 
+        </button>
+        {this.state.isDefault ? (
+          <CardDefault 
+            name={item.name}
+            image_url={item.image_url}
+            description={item.description}/>
+          ) : (
+          <CardFullInfo 
+            name={item.name}
+            description={item.description}
+            abv={item.abv}
+            contributed_by={item.contributed_by}
+            brewers_tips={item.brewers_tips}
+            first_brewed={item.first_brewed}
+            food_pairing={item.food_pairing}
+            ibu={item.ibu}
+            ph={item.ph}
+            srm={item.srm}
+            tagline={item.tagline}
+          />)}
+      </div>
+    )
+  }
+}
+
+function CardDefault(props) {
   return (
-    <div class="card">
-      <button class="showButton" onClick={props.onClick}>
-        ShowFullInfo
-      </button>
-      <div class="nameDescriptions">
+    <React.Fragment>
+      <div className="nameDescriptions">
         <b> {props.name} </b>
         <span>{props.description}</span>
       </div>
       <img alt={props.name} src={props.image_url} />
-    </div>
+    </React.Fragment>
   );
 }
 
@@ -54,62 +67,79 @@ function CardFullInfo(props) {
   //TODO: indgredienc should be added, method, volume
   return (
     // need to delete id here
-    <div class="card">
-      <button class="showButton" onClick={props.onClick}>
-        {" "}
-        / Back
-      </button>
-      <b> Name: {props.name} </b>
-      <span>
-        <b>Description:</b> {props.description}
-      </span>
-      <span>
-        <b>ABV:</b> {props.abv}
-      </span>
-      <span>
-        <b>Contributed by:</b>
-        {props.contributed_by}
-      </span>{" "}
-      brewers_tips
-      <span>
-        <b>Brewer Tips:</b>
-        {props.brewers_tips}
-      </span>
-      <span>
-        <b>First Brewed:</b>
-        {props.first_brewed}
-      </span>
-      <span>
-        <b>FoodPairing:</b>
-        {props.food_pairing}
-      </span>
-      <span>
-        <b>IBU:</b>
-        {props.ibu}
-      </span>
-      <span>
-        <b>PH:</b>
-        {props.ph}
-      </span>
-      <span>
-        <b>SRM:</b>
-        {props.srm}
-      </span>
-      <span>
-        <b>Tag Line:</b>
-        {props.tagline}
-      </span>
-    </div>
+      <React.Fragment>
+        <b> Name: {props.name} </b>
+        <span>
+          <b>Description:</b> {props.description}
+        </span>
+        <span>
+          <b>ABV:</b> {props.abv}
+        </span>
+        <span>
+          <b>Contributed by:</b>
+          {props.contributed_by}
+        </span>{" "}
+        brewers_tips
+        <span>
+          <b>Brewer Tips:</b>
+          {props.brewers_tips}
+        </span>
+        <span>
+          <b>First Brewed:</b>
+          {props.first_brewed}
+        </span>
+        <span>
+          <b>FoodPairing:</b>
+          {props.food_pairing}
+        </span>
+        <span>
+          <b>IBU:</b>
+          {props.ibu}
+        </span>
+        <span>
+          <b>PH:</b>
+          {props.ph}
+        </span>
+        <span>
+          <b>SRM:</b>
+          {props.srm}
+        </span>
+        <span>
+          <b>Tag Line:</b>
+          {props.tagline}
+        </span>
+      </React.Fragment>
   );
+}
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+
+    this.props.filterCards(event.target.value)
+  }
+
+  render() {
+    return <input value = {this.state.value} onChange={this.handleChange}/>
+  }
 }
 
 function DropDownToSort() {
   return (
-    <div class="dropdown">
-      <button class="dropbtn">Dropdown</button>
-      <div class="dropdown-content">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
+    <div className="dropdown">
+      <button className="dropbtn">Dropdown</button>
+      <div className="dropdown-content">
+        <a href="#">by name</a>
+        <a href="#">be volume</a>
         <a href="#">Link 3</a>
       </div>
     </div>
@@ -120,20 +150,35 @@ function Error(props) {
   return <div> {props.error} </div>;
 }
 
+const Statuses = {
+  INITIAL: "initial",
+  FAILED: "failed",
+  SUCCESSFUL: "successful"
+}
+
 export default class Catalog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       list: [],
-      statusRequest: "initial",
-      idToShowFullInfo: [],
+      statusRequest: Statuses.INITIAL,
+      filteredList: []
     };
-    this.showFullInfo = this.showFullInfo.bind(this);
-    this.hideFullInfo = this.hideFullInfo.bind(this);
+    this.filterCards = this.filterCards.bind(this)
   }
 
   componentDidMount() {
     this.getListProd("https://api.punkapi.com/v2/beers");
+  }
+
+  filterCards(value) {
+    this.setState((state) => {
+      return {
+        filteredList: state.list.filter((item) => {
+          return item.name.includes(value) || item.description.includes(value)
+        })
+      }
+    })
   }
 
   async getListProd(url) {
@@ -143,46 +188,38 @@ export default class Catalog extends React.Component {
     if (response.ok) {
       this.setState({
         list: results,
-        statusRequest: "successful",
+        filteredList: results,
+        statusRequest: Statuses.SUCCESSFUL,
       });
     } else {
       this.setState({
-        statusRequest: "failed",
+        statusRequest: Statuses.FAILED,
       });
     }
   }
 
-  showFullInfo(id) {
-    let arrIds = this.state.idToShowFullInfo.slice(0);
-    arrIds.push(id);
-    this.setState({
-      idToShowFullInfo: arrIds,
-    });
-    console.log("showFullInfo Works", this.state.idToShowFullInfo);
-  }
-
-  hideFullInfo(id) {
-    let arrIds = this.state.idToShowFullInfo.splice(id, 1);
-    this.setState({
-      idToShowFullInfo: arrIds,
-    });
-    console.log("hideFullInfoWorks");
-  }
 
   render() {
     console.log("state in render", this.state.list);
     console.log("stateID", this.state.idToShowFullInfo);
     return (
       <div>
-        <DropDownToSort />
-        <ShowListProd
-          data={this.state.list}
-          arrIds={this.state.idToShowFullInfo}
-          onClickShowDetails={(id) => this.showFullInfo(id)}
-          onClickBack={(id) => this.hideFullInfo(id)}
-        />
         {this.state.statusRequest ===
-          "Failed: Somethint wrong with the server" && (
+          Statuses.INITIAL && (
+          <p style={{textAlign: 'center', padding: 20}}>Loading...</p>
+        )}
+        {this.state.statusRequest ===
+          Statuses.SUCCESSFUL && (
+            <React.Fragment>
+              <DropDownToSort />
+              <Search filterCards={this.filterCards}/>
+              <Cards
+                data={this.state.filteredList}
+              />
+            </React.Fragment>
+        )}
+        {this.state.statusRequest ===
+          Statuses.FAILED && (
           <Error error={this.state.statusRequest} />
         )}
       </div>
