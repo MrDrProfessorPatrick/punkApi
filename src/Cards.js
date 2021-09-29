ba8faa431af304deffd8fc0bbd42c2078d6f3756
Zokof <vip.salm2@gmail.com>
2021-09-30 02:28:15 +0300

    Updates

--- src/Cards.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import {Card} from './Card'
-
-
-export function Cards(props) {
-    console.log("props.props", props.data); // list with arrays
-    return props.data.map((item) => <Card key={item.id} item={item} />);
-  }
--- src/PopUpForm.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export function PopUpForm(props){
-    return(<span className = "popUpForm">{props.message}</span>)
-    
-}
